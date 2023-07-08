ea6e54261ef4763734c334858d3db2b154113f4d
XIAOYU XIONG <xxyfh2@outlook.com>
2023-07-09 01:35:23 +0800

    finish 3D animation

--- Objects.py
+++ Objects.py
@@ -1,1 +1,71 @@
 import taichi as ti
+
+@ti.data_oriented
+class ClothSystem:
+    def __init__(self, number, position, velocity, forces, time = 0.0):
+        self.number = number
+        self.position = position
+        self.velocity = velocity
+        self.forces = forces
+        self.time = time
+
+    @ti.func
+    def ClearForces(self):
+        for i in range(self.number):
+            for j in range(self.number):
+                self.forces[i, j] = ti.Vector([0.0,0.0,0.0])
+    
+    @ti.func
+    def ApplyGravity(self):
+        for i in range(self.number):
+            for j in range(self.number):
+                self.forces[i, j] += ti.Vector([0.0,-9.8,0.0])
+
+    @ti.func
+    def ApplySpring(self, k_s:ti.f32, gamma_s:ti.f32, k_d:ti.f32, gamma_d:ti.f32, k_b:ti.f32, gamma_b:ti.f32, l:ti.f32):
+        for i in range(self.number):
+            for j in range(self.number):
+                if(i>0):
+                    self.forces[i, j] += (k_s*(ti.Vector.norm(self.position[i-1, j]-self.position[i, j])-l)+gamma_s*ti.Vector.dot(self.velocity[i-1, j]-self.velocity[i, j], self.position[i-1, j]-self.position[i, j])//ti.Vector.norm(self.position[i-1, j]-self.position[i, j]))*(self.position[i-1, j]-self.position[i, j])/ti.Vector.norm(self.position[i-1, j]-self.position[i, j])
+                if(i<self.number-1):
+                    self.forces[i, j] += (k_s*(ti.Vector.norm(self.position[i+1, j]-self.position[i, j])-l)+gamma_s*ti.Vector.dot(self.velocity[i+1, j]-self.velocity[i, j], self.position[i+1, j]-self.position[i, j])/ti.Vector.norm(self.position[i+1, j]-self.position[i, j]))*(self.position[i+1, j]-self.position[i, j])/ti.Vector.norm(self.position[i+1, j]-self.position[i, j])
+                if(j>0):
+                    self.forces[i, j] += (k_s*(ti.Vector.norm(self.position[i, j-1]-self.position[i, j])-l)+gamma_s*ti.Vector.dot(self.velocity[i, j-1]-self.velocity[i, j], self.position[i, j-1]-self.position[i, j])/ti.Vector.norm(self.position[i, j-1]-self.position[i, j]))*(self.position[i, j-1]-self.position[i, j])/ti.Vector.norm(self.position[i, j-1]-self.position[i, j])
+                if(j<self.number-1):
+                    self.forces[i, j] += (k_s*(ti.Vector.norm(self.position[i, j+1]-self.position[i, j])-l)+gamma_s*ti.Vector.dot(self.velocity[i, j+1]-self.velocity[i, j], self.position[i, j+1]-self.position[i, j])/ti.Vector.norm(self.position[i, j+1]-self.position[i, j]))*(self.position[i, j+1]-self.position[i, j])/ti.Vector.norm(self.position[i, j+1]-self.position[i, j])
+                if(i>0 and j>0):
+                    self.forces[i, j] += (k_d*(ti.Vector.norm(self.position[i-1, j-1]-self.position[i, j])-l*ti.sqrt(2))+gamma_d*ti.Vector.dot(self.velocity[i-1, j-1]-self.velocity[i, j], self.position[i-1, j-1]-self.position[i, j])/ti.Vector.norm(self.position[i-1, j-1]-self.position[i, j]))*(self.position[i-1, j-1]-self.position[i, j])/ti.Vector.norm(self.position[i-1, j-1]-self.position[i, j])
+                if(i>0 and j<self.number-1):
+                    self.forces[i, j] += (k_d*(ti.Vector.norm(self.position[i-1, j+1]-self.position[i, j])-l*ti.sqrt(2))+gamma_d*ti.Vector.dot(self.velocity[i-1, j+1]-self.velocity[i, j], self.position[i-1, j+1]-self.position[i, j])/ti.Vector.norm(self.position[i-1, j+1]-self.position[i, j]))*(self.position[i-1, j+1]-self.position[i, j])/ti.Vector.norm(self.position[i-1, j+1]-self.position[i, j])
+                if(i<self.number-1 and j>0):
+                    self.forces[i, j] += (k_d*(ti.Vector.norm(self.position[i+1, j-1]-self.position[i, j])-l*ti.sqrt(2))+gamma_d*ti.Vector.dot(self.velocity[i+1, j-1]-self.velocity[i, j], self.position[i+1, j-1]-self.position[i, j])/ti.Vector.norm(self.position[i+1, j-1]-self.position[i, j]))*(self.position[i+1, j-1]-self.position[i, j])/ti.Vector.norm(self.position[i+1, j-1]-self.position[i, j])
+                if(i<self.number-1 and j<self.number-1):
+                    self.forces[i, j] += (k_d*(ti.Vector.norm(self.position[i+1, j+1]-self.position[i, j])-l*ti.sqrt(2))+gamma_d*ti.Vector.dot(self.velocity[i+1, j+1]-self.velocity[i, j], self.position[i+1, j+1]-self.position[i, j])/ti.Vector.norm(self.position[i+1, j+1]-self.position[i, j]))*(self.position[i+1, j+1]-self.position[i, j])/ti.Vector.norm(self.position[i+1, j+1]-self.position[i, j])
+                if(i>1):
+                    self.forces[i, j] += (k_b*(ti.Vector.norm(self.position[i-2, j]-self.position[i, j])-2*l)+gamma_b*ti.Vector.dot(self.velocity[i-2, j]-self.velocity[i, j], self.position[i-2, j]-self.position[i, j])/ti.Vector.norm(self.position[i-2, j]-self.position[i, j]))*(self.position[i-2, j]-self.position[i, j])/ti.Vector.norm(self.position[i-2, j]-self.position[i, j])
+                if(i<self.number-2):
+                    self.forces[i, j] += (k_b*(ti.Vector.norm(self.position[i+2, j]-self.position[i, j])-2*l)+gamma_b*ti.Vector.dot(self.velocity[i+2, j]-self.velocity[i, j], self.position[i+2, j]-self.position[i, j])/ti.Vector.norm(self.position[i+2, j]-self.position[i, j]))*(self.position[i+2, j]-self.position[i, j])/ti.Vector.norm(self.position[i+2, j]-self.position[i, j])
+                if(j>1):
+                    self.forces[i, j] += (k_b*(ti.Vector.norm(self.position[i, j-2]-self.position[i, j])-2*l)+gamma_b*ti.Vector.dot(self.velocity[i, j-2]-self.velocity[i, j], self.position[i, j-2]-self.position[i, j])/ti.Vector.norm(self.position[i, j-2]-self.position[i, j]))*(self.position[i, j-2]-self.position[i, j])/ti.Vector.norm(self.position[i, j-2]-self.position[i, j])
+                if(j<self.number-2):
+                    self.forces[i, j] += (k_b*(ti.Vector.norm(self.position[i, j+2]-self.position[i, j])-2*l)+gamma_b*ti.Vector.dot(self.velocity[i, j+2]-self.velocity[i, j], self.position[i, j+2]-self.position[i, j])/ti.Vector.norm(self.position[i, j+2]-self.position[i, j]))*(self.position[i, j+2]-self.position[i, j])/ti.Vector.norm(self.position[i, j+2]-self.position[i, j])
+
+    @ti.func
+    def ApplyDamping(self, damp:ti.f32):
+        for i in range(self.number):
+            for j in range(self.number):
+                self.forces[i, j] -= damp*self.velocity[i, j]
+
+    @ti.func
+    def GroundCollision(self, i:int, j:int, ground:ti.f32):
+        if(self.position[i, j][1] <= ground):
+            self.position[i, j][1] = ground
+            self.velocity[i, j][1] -= 1.3*ti.Vector.dot(self.velocity[i, j], ti.Vector([0.0, 1.0, 0.0]))
+
+    @ti.func
+    def BallCollision(self, i:int, j:int, xc:ti.f32, yc:ti.f32, zc:ti.f32, r:ti.f32):
+        center = ti.Vector([xc, yc, zc])
+        if(ti.Vector.norm(self.position[i, j] - center) <= r):
+            self.position[i, j] = center + r*(self.position[i, j] - center)/ti.Vector.norm(self.position[i, j] - center)
+            self.velocity[i, j] -= 1.5*ti.Vector.dot(self.velocity[i, j], self.position[i, j] - center)*(self.position[i, j] - center)/ti.Vector.norm(self.position[i, j] - center)
+                
---
+++ main.py
@@ -0,0 +1,102 @@
+import taichi as ti
+import numpy as np
+from Objects import *
+
+ti.init(arch=ti.cpu)
+
+N = 64
+position = ti.Vector.field(3, dtype=ti.f32, shape=(N, N))
+velocity = ti.Vector.field(3, dtype=ti.f32, shape=(N, N))
+forces = ti.Vector.field(3, dtype=ti.f32, shape=(N, N))
+k_s = 4e3
+gamma_s = 3e3
+k_d = 4e3 / ti.sqrt(2)
+gamma_d = 3e3 / ti.sqrt(2)
+k_b = 4e3/2
+gamma_b = 3e3/2
+
+num_triangles = (N - 1) * (N - 1) * 2
+indices = ti.field(int, shape=num_triangles * 3)
+vertices = ti.Vector.field(3, dtype=float, shape=N * N)
+colors = ti.Vector.field(3, dtype=float, shape=N * N)
+
+# Initialize
+@ti.kernel
+def initialize():
+    for i in range(N):
+        for j in range(N):
+            position[i, j] = ti.Vector([i / float(N), 2.0, j/float(N)])
+            velocity[i, j] = ti.Vector([0.0, 0.0, 0.0])
+            forces[i, j] = ti.Vector([0.0, 0.0, 0.0])
+
+@ti.kernel
+def initialize_mesh_indices():
+    for i, j in ti.ndrange(N - 1, N - 1):
+        quad_id = (i * (N - 1)) + j
+        # 1st triangle of the square
+        indices[quad_id * 6 + 0] = i * N + j
+        indices[quad_id * 6 + 1] = (i + 1) * N + j
+        indices[quad_id * 6 + 2] = i * N + (j + 1)
+        # 2nd triangle of the square
+        indices[quad_id * 6 + 3] = (i + 1) * N + j + 1
+        indices[quad_id * 6 + 4] = i * N + (j + 1)
+        indices[quad_id * 6 + 5] = (i + 1) * N + j
+
+    for i, j in ti.ndrange(N, N):
+        if (i // 4 + j // 4) % 2 == 0:
+            colors[i * N + j] = (0.22, 0.72, 0.52)
+        else:
+            colors[i * N + j] = (1, 0.334, 0.52)
+
+@ti.kernel
+def initialize_vertices():
+    for i, j in ti.ndrange(N, N):
+        vertices[i * N + j] = position[i, j]
+
+initialize()
+initialize_mesh_indices()
+initialize_vertices()
+
+system = ClothSystem(N, position, velocity, forces)
+
+@ti.kernel
+def substep(h:ti.f32):
+    system.ClearForces()
+    system.ApplyGravity()
+    system.ApplySpring(k_s, gamma_s, k_d, gamma_d, k_b, gamma_b, 1 / float(N))
+    system.ApplyDamping(6.0)
+    for i in range(N):
+        for j in range(N):
+            system.position[i, j] += system.velocity[i, j]*h
+            system.velocity[i, j] += system.forces[i, j]*h
+            vertices[i * N + j] = system.position[i, j]
+            system.GroundCollision(i,j,0.0)
+            system.BallCollision(i,j,0.35,0.6,0.35,0.3)
+    #system.time += h
+
+window = ti.ui.Window("Taichi Cloth Simulation on GGUI", (1024, 1024),
+                      vsync=True)
+canvas = window.get_canvas()
+canvas.set_background_color((1, 1, 1))
+scene = ti.ui.Scene()
+camera = ti.ui.Camera()
+
+ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
+ball_center[0] = [0.35, 0.6, 0.35]
+
+while window.running:
+    for i in range(180):
+        substep(0.00007)
+    camera.position(0, 1.0, 3.0)
+    camera.lookat(0.5, 1.0, 0.5)
+    scene.set_camera(camera)
+
+    scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
+    scene.ambient_light((0.5, 0.5, 0.5))
+    scene.mesh(vertices,
+               indices=indices,
+               per_vertex_color=colors,
+               two_sided=True)
+    scene.particles(ball_center, radius=0.3*0.93, color=(0.5, 0.42, 0.8))
+    canvas.scene(scene)
+    window.show()
